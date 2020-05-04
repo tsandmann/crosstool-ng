7a8c7a9e9dbbd14e1368b2db1b415b8379879dc0
Timo Sandmann <mail@timosandmann.de>
2020-05-04 23:09:12 +0200

    setup source file updated

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 # source this file so setup your environment correct on macOS
-export PATH=$HOME/usr/local/bin:$PATH
+export PATH=$PATH:$HOME/usr/local/bin:/usr/local/opt/binutils/bin:/usr/local/opt/ncurses/bini
+export PKG_CONFIG_PATH="/usr/local/opt/ncurses/lib/pkgconfig"
+export LDFLAGS="-L/usr/local/opt/ncurses/lib"
+export CPPFLAGS="-I/usr/local/opt/ncurses/include"
 
