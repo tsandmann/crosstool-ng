a114d2cf2615658a4ac30ec1b3c0365c14914882
Timo Sandmann <mail@timosandmann.de>
2021-11-11 17:56:31 +0100

    homebrew prefix

--- setup.sh
+++ setup.sh
@@ -1,6 +1,10 @@
 # source this file so setup your environment correct on macOS
-export PATH=$PATH:$HOME/usr/local/bin:/usr/local/opt/binutils/bin:/usr/local/opt/ncurses/bini
-export PKG_CONFIG_PATH="/usr/local/opt/ncurses/lib/pkgconfig"
-export LDFLAGS="-L/usr/local/opt/ncurses/lib"
-export CPPFLAGS="-I/usr/local/opt/ncurses/include"
+export PATH=$PATH:$HOME/usr/local/bin
+export PATH=$(brew --prefix)/opt/binutils/bin:$PATH
+export PATH=$(brew --prefix)/opt/ncurses/bin:$PATH
+export PATH=$(brew --prefix)/opt/ccache/libexec:$PATH
+export PATH=$(brew --prefix)/opt/bison/bin/:$PATH
+export PKG_CONFIG_PATH="$(brew --prefix)/opt/ncurses/lib/pkgconfig"
+export LDFLAGS="-L$(brew --prefix)/opt/ncurses/lib"
+export CPPFLAGS="-I$(brew --prefix)/opt/ncurses/include"
 
